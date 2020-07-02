52a08046272aa8fe24c60bfd36a08402282f524b
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2020-07-02 21:02:25 +0200

    make hud form unclickable, change hud form window title

--- WtHud2/HUDForm.Designer.cs
+++ WtHud2/HUDForm.Designer.cs
@@ -37,24 +37,26 @@
             this.HUDLabel.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HUDLabel.ForeColor = System.Drawing.Color.White;
             this.HUDLabel.Location = new System.Drawing.Point(0, 0);
+            this.HUDLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.HUDLabel.Name = "HUDLabel";
-            this.HUDLabel.Size = new System.Drawing.Size(96, 27);
+            this.HUDLabel.Size = new System.Drawing.Size(76, 22);
             this.HUDLabel.TabIndex = 0;
             this.HUDLabel.Text = "label1";
             // 
             // HUDForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.HUDLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "HUDForm";
-            this.Text = "HUDForm";
+            this.Text = "WTHUD OSD";
             this.TopMost = true;
             this.TransparencyKey = System.Drawing.SystemColors.ControlDarkDark;
             this.ResumeLayout(false);
--- WtHud2/HUDForm.cs
+++ WtHud2/HUDForm.cs
@@ -8,5 +8,16 @@ namespace WtHud2
         {
             InitializeComponent();
         }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams createParams = base.CreateParams;
+                createParams.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+
+                return createParams;
+            }
+        }
     }
 }
