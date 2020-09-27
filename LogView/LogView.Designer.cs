c3f0a6d3c55750efcd1ac239bdad4de9c690311c
wysiwyng <4764286+wysiwyng@users.noreply.github.com>
2020-09-27 23:58:46 +0200

    fix logview

--- LogView/LogView.Designer.cs
+++ LogView/LogView.Designer.cs
@@ -38,10 +38,9 @@
             // LoadLogBtn
             // 
             this.LoadLogBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.LoadLogBtn.Location = new System.Drawing.Point(16, 511);
-            this.LoadLogBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.LoadLogBtn.Location = new System.Drawing.Point(12, 406);
             this.LoadLogBtn.Name = "LoadLogBtn";
-            this.LoadLogBtn.Size = new System.Drawing.Size(100, 28);
+            this.LoadLogBtn.Size = new System.Drawing.Size(75, 23);
             this.LoadLogBtn.TabIndex = 0;
             this.LoadLogBtn.Text = "Load Log...";
             this.LoadLogBtn.UseVisualStyleBackColor = true;
@@ -56,12 +55,11 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.PlotConfigDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.PlotConfigDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.PlotConfigDGV.Location = new System.Drawing.Point(16, 15);
-            this.PlotConfigDGV.Margin = new System.Windows.Forms.Padding(4);
+            this.PlotConfigDGV.Location = new System.Drawing.Point(12, 12);
             this.PlotConfigDGV.Name = "PlotConfigDGV";
             this.PlotConfigDGV.RowHeadersVisible = false;
             this.PlotConfigDGV.RowHeadersWidth = 51;
-            this.PlotConfigDGV.Size = new System.Drawing.Size(325, 489);
+            this.PlotConfigDGV.Size = new System.Drawing.Size(244, 388);
             this.PlotConfigDGV.TabIndex = 3;
             this.PlotConfigDGV.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.PlotConfigDGV_CellMouseUp);
             // 
@@ -71,11 +69,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TelemPlotView.BackColor = System.Drawing.SystemColors.Control;
-            this.TelemPlotView.Location = new System.Drawing.Point(349, 15);
-            this.TelemPlotView.Margin = new System.Windows.Forms.Padding(4);
+            this.TelemPlotView.Location = new System.Drawing.Point(262, 12);
             this.TelemPlotView.Name = "TelemPlotView";
             this.TelemPlotView.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.TelemPlotView.Size = new System.Drawing.Size(701, 521);
+            this.TelemPlotView.Size = new System.Drawing.Size(510, 414);
             this.TelemPlotView.TabIndex = 4;
             this.TelemPlotView.Text = "plotView1";
             this.TelemPlotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
@@ -84,10 +81,12 @@
             // 
             // ExportCSVBtn
             // 
+            this.ExportCSVBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.ExportCSVBtn.Enabled = false;
-            this.ExportCSVBtn.Location = new System.Drawing.Point(123, 511);
+            this.ExportCSVBtn.Location = new System.Drawing.Point(92, 406);
+            this.ExportCSVBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ExportCSVBtn.Name = "ExportCSVBtn";
-            this.ExportCSVBtn.Size = new System.Drawing.Size(100, 28);
+            this.ExportCSVBtn.Size = new System.Drawing.Size(75, 23);
             this.ExportCSVBtn.TabIndex = 5;
             this.ExportCSVBtn.Text = "Export CSV...";
             this.ExportCSVBtn.UseVisualStyleBackColor = true;
@@ -95,14 +94,14 @@
             // 
             // LogView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(784, 441);
             this.Controls.Add(this.ExportCSVBtn);
             this.Controls.Add(this.TelemPlotView);
             this.Controls.Add(this.PlotConfigDGV);
             this.Controls.Add(this.LoadLogBtn);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(800, 480);
             this.Name = "LogView";
             this.Text = "WTHUD Log Viewer";
             this.Load += new System.EventHandler(this.LogView_Load);
